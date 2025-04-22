38f620ff9b6a7546908c5a8165c527660780e000
Nano Miratus <nanomiratus@gmail.com>
2025-04-22 17:49:36 +0200

    Initial Commit

---
+++ shapre/shapreApp.swift
@@ -0,0 +1,17 @@
+//
+//  shapreApp.swift
+//  shapre
+//
+//  Created by Nano Miratus on 22.04.25.
+//
+
+import SwiftUI
+
+@main
+struct shapreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ shapreTests/shapreTests.swift
@@ -0,0 +1,17 @@
+//
+//  shapreTests.swift
+//  shapreTests
+//
+//  Created by Nano Miratus on 22.04.25.
+//
+
+import Testing
+@testable import shapre
+
+struct shapreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
